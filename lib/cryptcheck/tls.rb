299725996bdebf28987ac7e168b1ee4b90b343b3
aeris <aeris@imirhil.fr>
2016-12-17 14:39:43 +0100

    Display curve for ECC key

--- lib/cryptcheck/tls.rb
+++ lib/cryptcheck/tls.rb
@@ -9,19 +9,24 @@ module CryptCheck
 
 		def self.colorize(cipher)
 			colors = case
-						 when /^SSL/ =~ cipher then :critical
-						 when :TLSv1_2 == cipher then :good
+						 when /^SSL/ =~ cipher
+							 :critical
+						 when :TLSv1_2 == cipher
+							 :good
 					 end
 			cipher.to_s.colorize colors
 		end
 
 		def self.key_to_s(key)
-			type_color = case key.type
-							 when :ecc then :good
-							 when :dh then :warning
-							 when :dsa then :critical
+			size, color = case key.type
+							 when :ecc
+								 ["#{key.group.curve_name} #{key.size}", :good]
+							 when :dh
+								 [key.size, :warning]
+							 when :dsa
+								 [key.size, :critical]
 						 end
-			"#{key.type.to_s.upcase.colorize type_color} #{key.size.to_s.colorize key.status} bits"
+			"#{key.type.to_s.upcase.colorize color} #{size.to_s.colorize key.status} bits"
 		end
 	end
 end
