cdf9f62534fb7ff31df84d3e55766c62f4c6b6a7
aeris <aeris@imirhil.fr>
2017-04-15 18:14:03 +0200

    Fix check_https

--- bin/check_https.rb
+++ bin/check_https.rb
@@ -3,13 +3,4 @@ $:.unshift File.expand_path File.join File.dirname(__FILE__), '../lib'
 require 'rubygems'
 require 'bundler/setup'
 require 'cryptcheck'
-
-name = ARGV[0] || 'index'
-file = ::File.join 'output', "#{name}.yml"
-if ::File.exist? file
-	::CryptCheck::Logger.level = ENV['LOG'] || :none
-	::CryptCheck::Tls::Https.analyze_file file, "output/#{name}.html"
-else
-	::CryptCheck::Logger.level = ENV['LOG'] || :info
-	::CryptCheck::Tls::Https.analyze ARGV[0], (ARGV[1] || 443)
-end
+::CryptCheck::Tls::Https::Host.new ARGV[0], ARGV.fetch(1, 443)
